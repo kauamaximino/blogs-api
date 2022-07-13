27fce39df92b63ee98d8acad64941e1266767bc3
Kauã Maximino <kaua_maximino@outlook.com>
2022-07-13 19:56:50 -0300

    feat: requisito 2 criado

---
+++ src/database/models/user.js
@@ -0,0 +1,26 @@
+'use scrict'
+
+const User = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  }, {
+    tableName: 'Users',
+    timestamps: false,
+  });
+
+  User.associate = (db) => {
+    User.hasMany(db.BlogPost, { as: 'blogPost', foreignKey: 'userId' });
+  };
+  
+  return User
+};
+
+module.exports = User;
+
