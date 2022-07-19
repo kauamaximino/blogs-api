a47ea3fe81f5934af2247512a016a20d794617cc
Kauã Maximino <kaua_maximino@outlook.com>
2022-07-19 10:40:08 -0300

    refactor: removendo middleware desnecessário

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,11 +1,9 @@
 const loginValidate = require('./login.validate');
 const insertUserValidate = require('./insertUser.validate');
 const tokenValidate = require('./token.validate');
-const insertCategory = require('./insertCategory.validate');
 
 module.exports = {
   loginValidate,
   insertUserValidate,
   tokenValidate,
-  insertCategory,
 };
